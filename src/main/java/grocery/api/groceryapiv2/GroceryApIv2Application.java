a3f6566055bee50f38c2494d1dc542243c96f66b
GPA0VERKD <hungthui27@outlook.com>
2024-04-29 19:03:59 -0700

    Init Grocery API v2

---
+++ src/main/java/grocery/api/groceryapiv2/GroceryApIv2Application.java
@@ -0,0 +1,13 @@
+package grocery.api.groceryapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryApIv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryApIv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/grocery/api/groceryapiv2/GroceryApIv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package grocery.api.groceryapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryApIv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
